fb934f98cffb812ca18d3ca309e2ade62c7472ce
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-15 14:33:48 +0100

    dispatching an action that would always reduce 1 when the decrease button is clicked

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -15,6 +15,7 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
+    dispatch(-1);
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
   };
