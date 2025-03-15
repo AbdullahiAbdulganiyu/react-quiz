ceb9d61067cacf475345ecc34957b7e5518a3319
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-15 15:02:14 +0100

    commenting some code

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -2,9 +2,9 @@ import { useReducer, useState } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
-  if (action.type === "inc") return state + 1;
-  if (action.type === "dec") return state - 1;
-  if (action.type === "setCount") return action.payload;
+  // if (action.type === "inc") return state + 1;
+  // if (action.type === "dec") return state - 1;
+  // if (action.type === "setCount") return action.payload;
 }
 
 function DateCounter() {
