c3ba0187d6209ce04681559637076cd0fe453e7f
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-16 15:01:02 +0100

    using the main component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ export default function App() {
   return (
     <div className="app">
       <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
