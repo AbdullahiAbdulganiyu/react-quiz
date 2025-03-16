e0398cd5dc405ece252db368effcc7a7cdd2689f
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-16 22:17:23 +0100

    dispatching an error

--- src/App.js
+++ src/App.js
@@ -11,7 +11,11 @@ function reducer(state, action) {
   switch (action.type) {
     case "dataRecieved":
       return { ...state, questions: action.payload, status: "ready" };
-
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
     default:
       throw new Error("Unknown action");
   }
@@ -24,7 +28,7 @@ export default function App() {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataRecieved", payload: data }))
-      .catch((err) => console.error("Error"));
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
 
   return (
