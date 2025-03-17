9888c92c00c4b191b89a9af0c9b6d1de5033feee
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-17 11:07:00 +0100

    passing question based on index

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestion={numQuestion} dispatch={dispatch} />
         )}
-        {status === "active" && <Questions />}
+        {status === "active" && <Questions qustion={questions[0]} />}
       </Main>
     </div>
   );
