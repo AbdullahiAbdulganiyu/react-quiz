3163b46719d47ea287e8729b651d362710de4e44
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-17 10:39:21 +0100

    recieving the prop and rendering on the UI

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -1,8 +1,8 @@
-function StartScreen() {
+function StartScreen({ numQuestion }) {
   return (
     <div className="start">
       <h2>Welcome to The React Quiz!</h2>
-      <h3>X questions to test your React mastery</h3>
+      <h3>{numQuestion} questions to test your React mastery</h3>
       <button>Let's Start</button>
     </div>
   );
