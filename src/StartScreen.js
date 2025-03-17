6c2a9da5b9a54669817b030168260ef5d356872f
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-17 10:42:07 +0100

    adding a classname to the button

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -3,7 +3,7 @@ function StartScreen({ numQuestion }) {
     <div className="start">
       <h2>Welcome to The React Quiz!</h2>
       <h3>{numQuestion} questions to test your React mastery</h3>
-      <button>Let's Start</button>
+      <button className="btn btn-ui">Let's Start</button>
     </div>
   );
 }
