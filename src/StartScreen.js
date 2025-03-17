e42f5d672bc7aabd077fd1e966d81680e60d6f1b
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-17 10:33:21 +0100

    fixing some element naming

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -3,7 +3,7 @@ function StartScreen() {
     <div className="start">
       <h2>Welcome to The React Quiz!</h2>
       <h3>X questions to test your React mastery</h3>
-      <buttton>Let's Start</buttton>
+      <button>Let's Start</button>
     </div>
   );
 }
