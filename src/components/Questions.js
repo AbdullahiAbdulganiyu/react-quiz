bada0d775d8a3f86d79546de4b772ca9088a36ad
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-17 14:22:01 +0100

    using the options component

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,14 +1,10 @@
+import Options from "./Options";
+
 function Questions({ question }) {
   return (
     <div>
       <h4>{question.question}</h4>
-      <div className="options">
-        {question.options.map((option) => (
-          <button className="btn btn-option" key={option}>
-            {option}
-          </button>
-        ))}
-      </div>
+      <Options />
     </div>
   );
 }
