0d28307f8b47d4bbcf5da11122b92598ebb84c70
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-17 10:17:22 +0100

    destructuring the state

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
