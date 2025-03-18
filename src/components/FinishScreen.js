9b0c9cdd2bdda0e5c846c4432504f9f2312c4085
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-18 11:29:37 +0100

    some fix to the finishscreen component

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -1,8 +1,8 @@
 function FinishScreen({ points, maxPossiblePoints }) {
   const percentage = (points / maxPossiblePoints) * 100;
   return (
-    <p>
-      You Scored {points} out of {maxPossiblePoints} {Math.ceil(percentage)}
+    <p className="results">
+      You Scored {points} out of {maxPossiblePoints} ({Math.ceil(percentage)}%)
     </p>
   );
 }
