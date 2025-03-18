e321870344ee60de2334a166bc6420a07da256b4
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-18 22:35:00 +0100

    using the useeffect to set a timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,5 +1,18 @@
-function Timer() {
-  return <div className="timer">Timer</div>;
+import { useEffect } from "react";
+
+function Timer(dispatch) {
+  return (
+    <div className="timer">
+      {useEffect(
+        function () {
+          setInterval(function () {
+            dispatch({ type: "tick" });
+          }, 1000);
+        },
+        [dispatch]
+      )}
+    </div>
+  );
 }
 
 export default Timer;
