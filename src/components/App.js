0286aac9171e0fe0a68e5797c33c897f9872a094
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-18 11:10:56 +0100

    getting the answer state and using it to determine the value of the progress

--- src/components/App.js
+++ src/components/App.js
@@ -79,6 +79,7 @@ export default function App() {
               numQuestions={numQuestions}
               points={points}
               maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
             />
             <Questions
               question={questions[index]}
