87d42298189ad18e483fe4eec3a465ca6a3fbaf1
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-03-19 10:29:12 +0100

    assigning the setInterval ti a variable id

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 function Timer({ dispatch, secondsRemaining }) {
   useEffect(
     function () {
-      setInterval(function () {
+      const id = setInterval(function () {
         dispatch({ type: "tick" });
       }, 1000);
     },
